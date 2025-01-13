7da6b8781bff1fa2746ace26af6c78d3bfac14b8
ImPanica <yarshevich89@gmail.com>
2025-01-13 12:43:58 +0300

    Added Valid palindrome

--- Algos/Program.cs
+++ Algos/Program.cs
@@ -4,6 +4,7 @@ using Algos.Is_Subsequence;
 using Algos.Majority_Element;
 using Algos.Move_Zeroes;
 using Algos.Reverse_Words_in_a_String;
+using Algos.Strings.Valid_Palindrome;
 
 namespace Algos;
 
@@ -11,53 +12,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        // // Ноль в конец https://leetcode.com/problems/move-zeroes/description/
-        // // [0,1,0,3,12]
-        // // nums = [0]
-        // var num = MoveZeros.Move(new int[] { 0,1,0,3,12 });
-        // num.ToList().ForEach(Console.WriteLine);
-        //
-        // Split();
-        //
-        // // Наибольше кол-в оповторений https://leetcode.com/problems/majority-element/description/
-        // // [3,2,3]
-        // // [2,2,1,1,1,2,2]
-        // var majorCount = MajorityElement.MajorityToElement(new[] { 2,2,1,1,1,2,2 });
-        // Console.WriteLine(majorCount);
-        //
-        // Split();
-        // // Поиск вхождения в строке https://leetcode.com/problems/is-subsequence/
-        // // s = "abc", t = "ahbgdc"
-        // // s = "axc", t = "ahbgdc"
-        // var isSubsequence = Subsequence.IsSubsequence("abc", "ahbgdc");
-        // Console.WriteLine(isSubsequence);
-        //
-        // Split();
-        // // Реверс слов с пробелами https://leetcode.com/problems/reverse-words-in-a-string/
-        // // "the sky is blue"
-        // // "  hello world  "
-        // // "a good   example"
-        // var reverceWords = ReverseWords.ReverseOptionTwo("a good   example");
-        // Console.WriteLine(reverceWords);
-        //
-        // Split();
-        // // Получение едениц в битовом представлении числа https://leetcode.com/problems/counting-bits/description/
-        // // Input: n = 5
-        // // Output: [0,1,1,2,1,2]
-        // // Input: n = 2
-        // // Output: [0,1,1]
-        //
-        // var result = CountingBits.CountBits(7);
-        // for (int i = 0; i < result.Length; i++)
-        // {
-        //     Console.Write($"{result[i]} ");
-        // }
+        // Ноль в конец https://leetcode.com/problems/move-zeroes/description/
+        // [0,1,0,3,12]
+        // nums = [0]
+        var num = MoveZeros.Move(new int[] { 0,1,0,3,12 });
+        num.ToList().ForEach(Console.WriteLine);
+        
+        Split();
+        
+        // Наибольше кол-в оповторений https://leetcode.com/problems/majority-element/description/
+        // [3,2,3]
+        // [2,2,1,1,1,2,2]
+        var majorCount = MajorityElement.MajorityToElement(new[] { 2,2,1,1,1,2,2 });
+        Console.WriteLine(majorCount);
+        
+        Split();
+        // Поиск вхождения в строке https://leetcode.com/problems/is-subsequence/
+        // s = "abc", t = "ahbgdc"
+        // s = "axc", t = "ahbgdc"
+        var isSubsequence = Subsequence.IsSubsequence("abc", "ahbgdc");
+        Console.WriteLine(isSubsequence);
+        
+        Split();
+        // Реверс слов с пробелами https://leetcode.com/problems/reverse-words-in-a-string/
+        // "the sky is blue"
+        // "  hello world  "
+        // "a good   example"
+        var reverceWords = ReverseWords.ReverseOptionTwo("a good   example");
+        Console.WriteLine(reverceWords);
+        
+        Split();
+        // Получение едениц в битовом представлении числа https://leetcode.com/problems/counting-bits/description/
+        // Input: n = 5
+        // Output: [0,1,1,2,1,2]
+        // Input: n = 2
+        // Output: [0,1,1]
+        
+        var result = CountingBits.CountBits(7);
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.Write($"{result[i]} ");
+        }
+        
+        Console.WriteLine("\n");
         
         Split();
         // Удаление дубликатов в отсортированном массиве https://leetcode.com/problems/remove-duplicates-from-sorted-array/
         // nums = [1,1,2]
         // nums = [0,0,1,1,1,2,2,3,3,4]
-        RemoveDuplicates.RemoveDuplicatesFromArray(new[] { 1, 1, 2 });
+        var uniqueElementCount = RemoveDuplicates.RemoveDuplicatesFromArray(new[] { 0,0,1,1,1,2,2,3,3,4 });
+        Console.WriteLine(uniqueElementCount);
+        
+        Split();
+        // Проверка на палиндром
+        // s = "A man, a plan, a canal: Panama"
+        // s = "race a car"
+        // s = "Marge, let's \"[went].\" I await {news} telegram."
+        // s ="0P"
+        var isPalindrome = ValidPalindrome.IsPalindromeThird("Marge, let's \"[went].\" I await {news} telegram.}");
+        Console.WriteLine(isPalindrome);
         Console.ReadKey();
     }
 
---
+++ Algos/Strings/Valid Palindrome/ValidPalindrome.cs
@@ -0,0 +1,104 @@
+﻿namespace Algos.Strings.Valid_Palindrome;
+
+public class ValidPalindrome
+{
+    #region First
+
+    public static bool IsPalindromeFirst(string s)
+    {
+        // Если длинна строки 1 то будет true
+        if(s.Length == 1) return true;
+        
+        // Устанавливаем указатели
+        var leftPosition = 0;
+        var rightPosition = s.Length-1;
+    
+        while (leftPosition <= rightPosition)
+        {
+            // Проверка, что символ слева буквенно-цифровой и позиция слева меньше позиции справа
+            while (leftPosition < rightPosition && !IsAlphaNumber(s[leftPosition]))
+            {
+                leftPosition++;
+            }
+            // Проверка, что символ справа буквенно-цифровой и позиция справа больше позиции слева
+            while (rightPosition > leftPosition && !IsAlphaNumber(s[rightPosition]))
+            {
+                rightPosition--;
+            }
+            // Сравниваем символы
+            if(char.ToLower(s[leftPosition]) != char.ToLower(s[rightPosition])) return false;
+            
+            // Изменяем значения указателей
+            leftPosition++;
+            rightPosition--;
+        }
+        // Возвращаем результат
+        return true;
+    }
+    
+    /// <summary>
+    /// Проверка является ли символ числом или латинским символом
+    /// </summary>
+    /// <param name="c">Символ для проверки</param>
+    /// <returns>Результат</returns>
+    private static bool IsAlphaNumber(char c)
+    {
+        return (c >= 97 && c <= 122 || c >= 48 && c <= 57 || c >= 65 && c <= 90);
+    }
+
+    #endregion
+
+    #region Second
+
+    public static bool IsPalindromeSecond(string s)
+    {
+        // Приводим к новой строке удаляя все кроме чисел и букв
+        var result = new string(s.ToLower().Where(c => char.IsLetterOrDigit(c)).ToArray());
+        // Делаем реверс строки и сравниваем
+        return result == new string(result.Reverse().ToArray());
+    }
+
+    #endregion
+
+    #region Third
+
+    /// <summary>
+    /// Проверка на палиндром используя встроенные методы
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static bool IsPalindromeThird(string s)
+    {
+        // Если длинна строки 1 то будет true
+        if(s.Length == 1) return true;
+        
+        // Устанавливаем указатели
+        var leftPosition = 0;
+        var rightPosition = s.Length-1;
+    
+        // Проходим по строке
+        while (leftPosition <= rightPosition)
+        {
+            // Проверка, что символ слева буквенно-цифровой и позиция слева меньше позиции справа
+            while (leftPosition < rightPosition && !char.IsLetterOrDigit(s[leftPosition]))
+            {
+                leftPosition++;
+            }
+            // Проверка, что символ справа буквенно-цифровой и позиция справа больше позиции слева
+            while (rightPosition > leftPosition && !char.IsLetterOrDigit(s[rightPosition]))
+            {
+                rightPosition--;
+            }
+            // Сравниваем символы
+            if(char.ToLower(s[leftPosition]) != char.ToLower(s[rightPosition])) return false;
+            
+            // Изменяем значения указателей
+            leftPosition++;
+            rightPosition--;
+        }
+        // Возвращаем результат
+        return true;
+    }
+
+    #endregion
+}
